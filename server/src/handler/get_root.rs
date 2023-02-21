20db61fa5a871867c8a17fe4eb36ee0fc75ec595
c0np4nn4 <c0np4nn4@gmail.com>
2023-02-21 15:28:41 +0900

    wip: pass `root` value

--- server/src/handler/get_root.rs
+++ server/src/handler/get_root.rs
@@ -8,43 +8,34 @@ struct GetRootRequest {
 }
 
 fn convert_u64_array_to_u8_vec(array_u64: [u64; 4]) -> Vec<u8> {
-    // use std::ptr;
-    // use std::slice;
-    // let slice_u64 = &array_u64[..];
-    // let num_bytes = slice_u64.len() * std::mem::size_of::<u64>();
-    // let mut vec_u8: Vec<u8> = Vec::with_capacity(num_bytes);
-    // let ptr_u64 = slice_u64.as_ptr();
-    // unsafe {
-    //     let ptr_u8 = vec_u8.as_mut_ptr();
-    //     let slice_u8 = slice::from_raw_parts_mut(ptr_u8, num_bytes);
-    //     ptr::copy_nonoverlapping(ptr_u64 as *const u8, slice_u8.as_mut_ptr(), num_bytes);
-    //     vec_u8.set_len(num_bytes);
-    // }
-    // vec_u8
     let mut res = Vec::<u8>::new();
+
+    let mask: [u64; 8] = [
+        0xff00_0000_0000_0000,
+        0x00ff_0000_0000_0000,
+        0x0000_ff00_0000_0000,
+        0x0000_00ff_0000_0000,
+        0x0000_0000_ff00_0000,
+        0x0000_0000_00ff_0000,
+        0x0000_0000_0000_ff00,
+        0x0000_0000_0000_00ff,
+    ];
 
     for i in 0..4 {
         let a = array_u64[i];
-        println!("a: {:064x?}", a);
-
-        let mask: [u64; 8] = [
-            0xff00_0000_0000_0000,
-            0x00ff_0000_0000_0000,
-            0x0000_ff00_0000_0000,
-            0x0000_00ff_0000_0000,
-            0x0000_0000_ff00_0000,
-            0x0000_0000_00ff_0000,
-            0x0000_0000_0000_ff00,
-            0x0000_0000_0000_00ff,
-        ];
+        println!("a: {:016x?}", a);
 
         for j in 0..8 {
-            let b: u8 = (((a & mask[j]) >> (8 * (7 - i))) & 0xff) as u8;
-            println!("b: {:02x?}", b);
+            let b: u8 = (((a & mask[j]) >> (8 * (7 - j))) & 0xff) as u8;
+            println!("\tb: {:02x?}", b);
+            //
+            res.push(b);
         }
     }
 
-    vec![]
+    res
+
+    // vec![]
 }
 
 pub async fn get_root(mut ctx: Context) -> Response {
